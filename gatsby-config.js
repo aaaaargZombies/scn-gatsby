98dc4bab258c9e023505a9583d95b0f96e22e2e8
aaaaargZombies <you@example.com>
2020-02-10 19:03:33 +0000

    update gatsby-config.js to reflect name change of plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,26 +5,26 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    {
-      resolve: "gatsby-source-starter",
-      options: {
-        centres: [
-          "DIY Space for London",
-          "Sumac Centre",
-          "The Autonomous Centre of Edinburgh",
-          "Cowley Club",
-          "Common House",
-          "The Field",
-          "Freedom Bookshop",
-          "Forest",
-          "LARC",
-          "Partisan",
-          "DIY Space for London",
-          "Star and Shadow Cinema",
-        ],
-      },
-    },
-  ],
+	/* Your site config here */
+	plugins: [
+		{
+			resolve: "gatsby-source-radar",
+			options: {
+				centres: [
+					"DIY Space for London",
+					"Sumac Centre",
+					"The Autonomous Centre of Edinburgh",
+					"Cowley Club",
+					"Common House",
+					"The Field",
+					"Freedom Bookshop",
+					"Forest",
+					"LARC",
+					"Partisan",
+					"DIY Space for London",
+					"Star and Shadow Cinema",
+				],
+			},
+		},
+	],
 }
