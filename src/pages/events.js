c00bb269c4d2064dada700a69dcdf5219aedad92
aaaaargZombies <you@example.com>
2020-02-12 15:14:37 +0000

    add a list of events to an events page:w

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,22 +3,33 @@ import { Link, graphql } from "gatsby"
 console.log("EVENTS PAGE")
 
 export default ({ data }) => {
-	console.log({ data })
+	console.log(data)
 	return (
 		<div>
 			<Link to="/">home</Link>
+			<ul>
+				{data.allEvent.nodes.map(n => (
+					<li>
+						<h2>{n.title}</h2>
+						<h6>@ {n.centre.title}</h6>
+						<img src="n.imageUrl" alt="event poster" />
+						<div dangerouslySetInnerHTML={{ __html: n.body.value }} />
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
 
 export const query = graphql`
-	query EventsPageQuery {
+	query {
 		allEvent {
 			nodes {
-				title
+				imageUrl
 				body {
 					value
 				}
+				title
 				centre {
 					title
 				}
